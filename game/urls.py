685da1ee08635db14689591c224d03ec3d1d2071
FoxMaySay <30768747+FoxMaySay@users.noreply.github.com>
2018-11-19 15:55:18 +0800

    Update urls.py

--- game/urls.py
+++ game/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.Home, name='home'),
-    path('404/', views.page404, name='404')
+    path('games/', views.GamesSum, name='games'),
+    path('<slug>', views.GameDetail, name='detail'),
+    path('faq/', views.FAQ, name='faq'),
+    path('contact-us/', views.ContactUs, name='contact-us'),
+    path('submit/', views.Submit, name='submit'),
+    path('404/', views.Page404, name='404'),
 ]
