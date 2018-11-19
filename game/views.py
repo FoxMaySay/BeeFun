dfd6e5a7c345308592a367d610d660cd52c5f395
FoxMaySay <30768747+FoxMaySay@users.noreply.github.com>
2018-11-19 15:55:41 +0800

    Update views.py

--- game/views.py
+++ game/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+
 def Home(request):
     return render(request, 'index.html')
 
-def page404(request):
+
+def GamesSum(request):
+    return render(request, 'games.html')
+
+
+def GameDetail(request):
+    return render(request, 'detail.html')
+
+
+def FAQ(request):
+    return render(request, 'faq.html')
+
+
+def ContactUs(request):
+    return render(request, 'contact.html')
+
+
+def Submit(request):
+    return render(request, 'submit.html')
+
+
+def Page404(request):
     return render(request, '404.html')
