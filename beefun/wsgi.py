a6c9c5248283cfe0a9eb1e20d89bc87fb68ad507
FoxMaySay <30768747+FoxMaySay@users.noreply.github.com>
2018-12-17 14:41:18 +0800

    Update wsgi.py

--- beefun/wsgi.py
+++ beefun/wsgi.py
@@ -7,10 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
-import os
-
+import os,sys,django
 from django.core.wsgi import get_wsgi_application
+from django.core.handlers.wsgi import  WSGIHandler
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beefun.settings')
+reload(sys)
 
-application = get_wsgi_application()
+sys.setdefaultencoding('utf-8')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "beefun.settings")  #变量testproject.settings为django项目下的settings
+
+django.setup()     #避免在虚拟环境下找不到django的app
+
+application = WSGIHandler()  #实例化一个WSGI application用作接受nginx服务器传递的envrion、start_response参数
